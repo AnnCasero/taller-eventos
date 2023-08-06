412849a96065536793e7ebb941d59db519fb3979
Anabel Casero <anabelcasero19@gmail.com>
2023-08-06 20:16:18 -0300

    update

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const botonesSaludo = document.querySelector('.myButton');
+
+  botonesSaludo.addEventListener('click', function() {
+      alert('Hola! Soy el div"');
+    });
+});

34974a6002bbd9fb78a4a9716506c5bb83404a01
Anabel Casero <anabelcasero19@gmail.com>
2023-09-01 21:13:44 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,8 +2,9 @@ function saludar() {
   alert('¡Hola!');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(event) {
   const divElement = document.getElementById("myButton");
+   event.stopPropagation();
 
   divElement.addEventListener('click', function() {
       alert('Hola! Soy el div');
